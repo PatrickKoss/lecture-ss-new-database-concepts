bed9fe4511b8d8188bae097acf548b8d65969da1
PatrickKoss <pati.koss@gmx.de>
2023-04-01 20:31:30 +0200

    raft mess around

--- patrick-db-raft/src/main.rs
+++ patrick-db-raft/src/main.rs
@@ -1,13 +1,19 @@
-use std::{sync::mpsc::{channel, RecvTimeoutError}, time::{Duration, Instant}};
+use std::{time::{Duration, Instant}};
+use std::collections::HashMap;
 
-use std::sync::Mutex;
-
-use raft::{Config, GetEntriesContext, raw_node::RawNode};
+use raft::{Config, GetEntriesContext, raw_node::RawNode, Result as RaftResult, Error as RaftError, Storage as RaftStorage};
 use raft::prelude::*;
 use slog::{Drain, Logger, o};
 use raft::eraftpb::Entry;
-use raft::storage::MemStorage;
-use regex::internal::Input;
+
+use std::sync::{Arc, RwLock};
+use tokio::sync::mpsc::{Receiver, Sender};
+use protobuf::CachedSize;
+use protobuf::UnknownFields;
+use protobuf::SingularPtrField;
+use bytes::Bytes;
+use tokio::sync::mpsc::channel;
+use tokio::sync::Mutex;
 
 fn create_logger() -> Logger {
     let decorator = slog_term::TermDecorator::new().build();
@@ -23,6 +29,7 @@ enum Msg {
         callback: Box<dyn Fn() + Send>,
     },
     Raft(Message),
+    RaftEntry(Entry),
 }
 
 fn handle_normal(entry: Entry) {
@@ -46,62 +53,121 @@ fn handle_committed_entries(committed_entries: Vec<Entry>) {
 }
 
 struct Storage {
-    term: Mutex<u64>,
-    first_index: Mutex<u64>,
-    last_index: Mutex<u64>,
+    hard_state: RwLock<HardState>,
+    conf_state: RwLock<ConfState>,
+    entries: RwLock<Vec<Entry>>,
+    snapshot: RwLock<Option<Snapshot>>,
 }
 
 impl Storage {
-    pub fn new() -> Self {
+    pub fn new(voters: Vec<u64>) -> Storage {
+        let mut conf_state = ConfState::default();
+        conf_state.set_voters(voters);
+
         Storage {
-            term: Mutex::new(0),
-            first_index: Mutex::new(1),
-            last_index: Mutex::new(1),
+            hard_state: RwLock::new(HardState::default()),
+            conf_state: RwLock::new(conf_state),
+            entries: RwLock::new(Vec::new()),
+            snapshot: RwLock::new(None),
         }
+    }
+
+    pub fn save_entries(&self, new_entries: &[Entry]) -> RaftResult<()> {
+        let mut entries = self.entries.write().unwrap();
+        for entry in new_entries {
+            entries.push(entry.clone());
+        }
+        Ok(())
+    }
+
+    pub fn update_hard_state(&self, new_hard_state: HardState) -> RaftResult<()> {
+        let mut hard_state = self.hard_state.write().unwrap();
+        *hard_state = new_hard_state;
+        Ok(())
+    }
+
+    pub fn update_conf_state(&self, new_conf_state: ConfState) -> RaftResult<()> {
+        let mut conf_state = self.conf_state.write().unwrap();
+        *conf_state = new_conf_state;
+        Ok(())
+    }
+
+    pub fn create_snapshot(&self, request_index: u64, term: u64, conf_state: ConfState) -> RaftResult<Snapshot> {
+        let conf_state_field:SingularPtrField<ConfState> = SingularPtrField::some(conf_state);
+        let snapshot_metadata = SnapshotMetadata {
+            index: request_index,
+            term,
+            cached_size: CachedSize::default(),
+            conf_state: conf_state_field,
+            unknown_fields: UnknownFields::default(),
+        };
+
+        let snap_shot_field: SingularPtrField<SnapshotMetadata> = SingularPtrField::some(snapshot_metadata);
+        let snapshot = Snapshot {
+            metadata: snap_shot_field,
+            data: Bytes::new(),
+            unknown_fields: UnknownFields::default(),
+            cached_size: CachedSize::default(), // You can store any relevant data for your application here.
+        };
+
+        {
+            let mut stored_snapshot = self.snapshot.write().unwrap();
+            *stored_snapshot = Some(snapshot.clone());
+        }
+
+        Ok(snapshot)
+    }
+
+    pub fn apply_snapshot(&self, snapshot: Snapshot) -> RaftResult<()> {
+        let mut stored_snapshot = self.snapshot.write().unwrap();
+        *stored_snapshot = Some(snapshot);
+        Ok(())
     }
 }
 
 impl raft::Storage for Storage {
-    fn initial_state(&self) -> raft::Result<RaftState> {
-        log::info!("initial_state");
-        let hard_state = HardState::default();
-        let mut conf_state = ConfState::default();
-        conf_state.set_voters(vec![1, 2, 3, 4, 5]);
+    fn initial_state(&self) -> RaftResult<RaftState> {
+        let hard_state = self.hard_state.read().unwrap().clone();
+        let conf_state = self.conf_state.write().unwrap().clone();
+
         Ok(RaftState {
             hard_state,
             conf_state,
         })
     }
 
-    fn entries(&self, low: u64, high: u64, max_size: impl Into<Option<u64>>, context: GetEntriesContext) -> raft::Result<Vec<Entry>> {
-        log::info!("entries: {:?}, {:?}, {:?}, {:?}", low, high, max_size.into(), context);
-        Ok(Vec::new())
+    fn entries(&self, low: u64, high: u64, _max_size: impl Into<Option<u64>>, _context: GetEntriesContext) -> RaftResult<Vec<Entry>> {
+        let entries = self.entries.read().unwrap();
+        let res = entries[low as usize..high as usize].to_vec();
+        Ok(res)
     }
 
-    fn term(&self, idx: u64) -> raft::Result<u64> {
-        log::info!("term: {:?}", idx);
-        let mut term = self.term.lock().unwrap();
-        *term += 1;
-        Ok(*term)
+    fn term(&self, idx: u64) -> RaftResult<u64> {
+        if idx == 0 {
+            return Ok(0);
+        }
+
+        let entries = self.entries.read().unwrap();
+        entries.get((idx as usize) - 1).map(|entry| entry.term).ok_or(RaftError::Store(raft::StorageError::Unavailable))
     }
 
-    fn first_index(&self) -> raft::Result<u64> {
-        log::info!("first_index");
-        let mut first_index = self.first_index.lock().unwrap();
-        *first_index += 1;
-        Ok(*first_index)
+    fn first_index(&self) -> RaftResult<u64> {
+        let entries = self.entries.read().unwrap();
+        Ok(entries.first().map(|entry| entry.index).unwrap_or(1))
     }
 
-    fn last_index(&self) -> raft::Result<u64> {
-        log::info!("last_index");
-        let mut last_index = self.last_index.lock().unwrap();
-        *last_index += 1;
-        Ok(*last_index)
+    fn last_index(&self) -> RaftResult<u64> {
+        let entries = self.entries.read().unwrap();
+        Ok(entries.last().map(|entry| entry.index).unwrap_or(0))
     }
 
-    fn snapshot(&self, request_index: u64, to: u64) -> raft::Result<Snapshot> {
-        log::info!("snapshot: {:?}, {:?}", request_index, to);
-        Ok(Snapshot::default())
+    fn snapshot(&self, _request_index: u64, _to: u64) -> RaftResult<Snapshot> {
+        let snapshot: Snapshot = match self.snapshot.read().unwrap().as_ref() {
+            Some(s) => s.clone(),
+            None => return Err(RaftError::Store(raft::StorageError::SnapshotOutOfDate)),
+        };
+
+        Ok(snapshot)
     }
 }
 
@@ -115,40 +181,76 @@ async fn main() {
     };
     config.validate().unwrap();
     let logger = create_logger();
-    let storage = MemStorage::new_with_conf_state((vec![1,2,3,4,5], vec![]));
-    // let storage = Storage::new();
-    let mut node = RawNode::new(&config, storage, &logger).unwrap();
+    // let storage = MemStorage::new_with_conf_state((vec![1,2,3,4,5], vec![]));
+    let node_channels = Arc::new(Mutex::new(HashMap::<u64, Sender<Msg>>::new()));
+    let voters = vec![1, 2, 3];
+    for voter in &voters {
+        let config = Config {
+            id: *voter,
+            heartbeat_tick: 1,
+            election_tick: 3,
+            ..Default::default()
+        };
+        let storage = Storage::new(voters.clone());
+        let node = RawNode::new(&config, storage, &logger).unwrap();
+        let (tx, rx) = channel::<Msg>(1000);
+        let mut node_channels_insert = node_channels.lock().await;
+        node_channels_insert.insert(*voter, tx.clone());
+        drop(node_channels_insert);
+        let node_channels_clone = node_channels.clone();
+        tokio::spawn(async move {
+            run_node(node, rx, node_channels_clone).await;
+        });
+    }
 
+    // Simulate a message coming down the stream.
+    let mut node_channels = node_channels.lock().await;
+    let sender_node = 1_u64;
+    if let Some(channel) = node_channels.get(&sender_node) {
+        let tx = channel.clone();
+        tokio::spawn(async move {
+            tokio::time::sleep(Duration::from_millis(5000)).await;
+            let mut interval = tokio::time::interval(Duration::from_millis(3000));
+            loop {
+                interval.tick().await;
+                if let Err(e) = tx.send(Msg::Propose {
+                    id: 1,
+                    callback: Box::new(|| ()),
+                }).await {
+                    log::warn!("Sending tick failed failed: {}", e);
+                }
+            }
+        });
+    }
+    drop(node_channels);
+
+    loop {
+        tokio::time::sleep(Duration::from_millis(1000)).await;
+    }
+}
+
+async fn run_node(mut node: RawNode<Storage>, mut rx: Receiver<Msg>, node_channels: Arc<Mutex<HashMap<u64, Sender<Msg>>>>) {
     // Ticking the Raft node
-    let (tx, rx) = channel();
     let timeout = Duration::from_millis(100);
     let mut remaining_timeout = timeout;
 
-    // Simulate a message coming down the stream.
-    tokio::spawn(async move {
-        tokio::time::sleep(Duration::from_millis(5000)).await;
-        let mut interval = tokio::time::interval(Duration::from_millis(3000));
-        loop {
-            interval.tick().await;
-            tx.send(Msg::Propose {
-                id: 1,
-                callback: Box::new(|| ()),
-            }).unwrap();
-        }
-    });
-
     loop {
         let now = Instant::now();
-        match rx.recv_timeout(remaining_timeout) {
-            Ok(Msg::Propose { id, callback: _ }) => {
+
+        match rx.recv().await {
+            Some(Msg::Propose { id, callback: _ }) => {
                 log::info!("Propose: {:?}", id);
                 if let Err(e) = node.propose(vec![], vec![id]) {
                     log::error!("Propose error: {:?}", e);
                 }
             }
-            Ok(Msg::Raft(m)) => node.step(m).unwrap(),
-            Err(RecvTimeoutError::Timeout) => (),
-            Err(RecvTimeoutError::Disconnected) => unimplemented!(),
+            Some(Msg::Raft(m)) => {
+                log::info!("got raft message: {:?}", m);
+                log::info!("entries: {:?}", m.get_entries());
+                node.step(m).unwrap()
+            },
+            Some(Msg::RaftEntry(entry)) => node.mut_store().save_entries(&[entry]).unwrap(),
+            None => (),
         }
 
         let elapsed = now.elapsed();
@@ -165,14 +267,22 @@ async fn main() {
 
         let mut ready = node.ready();
 
-        for _msg in ready.take_messages() {
-            log::info!("take_messages and send them to peers: {:?}", _msg);
-            // Send messages to other peers.
+        for msg in ready.take_messages() {
+            log::info!("take_messages and send them to peers: {:?}", msg);
+            let mut node_channels = node_channels.lock().await;
+            if let Some(channel) = node_channels.get(&msg.to) {
+                let tx = channel.clone();
+                if let Err(e) = tx.send(Msg::Raft(msg)).await {
+                    log::warn!("sending to channel for replication message failed: {}", e);
+                }
+            } else {
+                log::warn!("recipient not found for message: {:?}", msg);
+            }
+            drop(node_channels);
         }
 
         if !ready.snapshot().is_empty() {
             node.mut_store()
-                .wl()
                 .apply_snapshot(ready.snapshot().clone())
                 .unwrap();
         }
@@ -194,16 +304,25 @@ async fn main() {
         }
 
         if !ready.entries().is_empty() {
-            node.mut_store().wl().append(ready.entries()).unwrap();
+            node.mut_store().save_entries(ready.entries()).unwrap();
         }
 
         if let Some(hs) = ready.hs() {
-            node.mut_store().wl().set_hardstate(hs.clone());
+            node.mut_store().update_hard_state(hs.clone());
         }
 
-        for _msg in ready.take_persisted_messages() {
-            log::info!("take_persisted_messages and send them to peers: {:?}", _msg);
-            // Send persisted messages to other peers.
+        for msg in ready.take_persisted_messages() {
+            log::info!("take_persisted_messages and send them to peers: {:?}", msg);
+            let mut node_channels = node_channels.lock().await;
+            if let Some(channel) = node_channels.get(&msg.to) {
+                let tx = channel.clone();
+                if let Err(e) = tx.send(Msg::Raft(msg)).await {
+                    log::warn!("sending to channel for replication message failed: {}", e);
+                }
+            } else {
+                log::warn!("recipient not found for message: {:?}", msg);
+            }
+            drop(node_channels);
         }
 
         let mut light_rd = node.advance(ready);
